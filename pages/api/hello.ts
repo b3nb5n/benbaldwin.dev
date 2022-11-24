0b25925fb72e27784911e92b8ac83104b24ca801
Ben Baldwin <benbaldwin000@gmail.com>
2022-11-24 13:09:43 -0700

    astro setup

--- pages/api/hello.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-
-function handler(_: NextApiRequest, res: NextApiResponse) {
-	res.status(200).json({ text: 'Hello' })
-}
-
-export default handler
